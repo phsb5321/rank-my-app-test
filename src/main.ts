7a3d36ff3b2ed02ed2ab84f6beb9ab9d51364c58
Pedro Balbino <phsb5321@gmail.com>
2022-03-17 19:46:11 -0300

    Chore: Swagger && HttpModule are working

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Rank my app live code')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(3000);
+}
+bootstrap();
